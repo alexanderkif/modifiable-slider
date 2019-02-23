12d02592610b8e59e5b8c64e59659d31122dccdf
alexanderkif <alexander_kif@mail.ru>
2019-02-23 10:10:31 +0400

    changed dependencies position in package.json

--- demo/index.js
+++ demo/index.js
@@ -4,8 +4,6 @@ import "./favicon.ico";
 import "./constructor/constructor";
 import Slider from "../src/Slider";
 
-console.log("Run...");
-
 var dataJson = require('./data.json');
 [].forEach.call(document.getElementsByClassName(dataJson.sliderClass), element => {
     new Slider(element);
